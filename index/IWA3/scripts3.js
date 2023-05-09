08798c861bf6433a758cda1e517003bfc60ae27f
Mxolisi <m.makhubela@lab.learning.org>
2023-05-09 19:36:19 +0200

    imported footer

--- index/IWA3/scripts3.js
+++ index/IWA3/scripts3.js
@@ -1,8 +1,8 @@
 
 // scripts.js
 
-import {company} from './configurattion.js'
-import {year} from './configurattion.js'
+import  {company} from './configurattion.js'
+import  {year} from './configurattion.js'
 
 const message = '© ' + company + ' (' + year + ')'
 document.querySelector('footer').innerText = message
