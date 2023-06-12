655fd81dda77f40d2ae564d8cfb7ca635566bde8
Mxolisi <m.makhubela@lab.learning.org>
2023-06-12 10:01:03 +0200

    added objects

---
+++ index/IWA14/script14.js
@@ -0,0 +1,16 @@
+firstName = 'John';
+age = 35;
+let hobbys = 'Coding';
+
+const logTwice = () => {
+  console(parameter)
+  console(parameter)
+}
+
+function hobby () {
+  logTwice
+  console.log(`Hello, ${firstName} (${age}). I love ${hobbys}!`)
+}
+
+hobby()
+hobby()
